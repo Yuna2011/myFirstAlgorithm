cbaa6fd02846574393c552bf8eb66f526920a89f
Yuna2011 <yuna.shin1@education.nsw.gov.au>
2026-02-11 10:10:54 +1100

    addingItems
    Started on the addingItems function. I guess i'm nearly done. Just need to finish so that you can get to the checkout if you are done picking your items.

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-foodList = ["Meatpie", "Scroll"]
+foodList = ["1 - Meatpie", "2 - Scroll", "3 - Juice", "4 - Checkout"]
+Checkout = True
 
 Name = input("Enter student name : ")
 
+print(f"""Welcome {Name}!""")
 
+def addingItems():
+    while Checkout:
+        for item in foodList:
+            print(item)
+        choice = input("Enter the number of the item you would like : ")
+        if choice == 1:
+            print("You chose the Meatpie")
+        elif choice == 2:
+            print("You chose the scroll")
+        elif choice == 3:
+            print("You chose the juice")
+        elif choice == 4:
+            print("Would you like to go to the checkout?")
+#            checkoutChoice = input("Would you like to go to the Checkout? 1 for yes, 2 for no")
+#            if checkoutChoice ==  1:
+#                break   
+      
 
-
+addingItems()
