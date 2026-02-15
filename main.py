a9c332e3893e30b15a569959bbd4c357726d7bba
Yuna2011 <yuna.shin1@education.nsw.gov.au>
2026-02-16 01:22:49 +1100

    FInalised??
    I've got the basics down and didn't use specified functions as that just made the code more confusing for me. I might if I have time add where they can choose more than one option  and maybe add more options. I don't have a discount section yet as none of  my items go over $10 but if I add the ability to choose more than one item I will add it. For now the basics are done.

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
-foodList = ["1 - Meatpie", "2 - Scroll", "3 - Juice", "4 - Checkout"]
+foodList = ["1 - Meatpie, $5", "2 - Scroll, $10", "3 - Juice, $3", "4 - Checkout"]
 Checkout = True
+
+item_name = ""
+price = 0
 
 Name = input("Enter student name : ")
 
 print(f"""Welcome {Name}!""")
 
+def receipt(item_name, price):
+    print("\n----- RECEIPT -----")
+    print("Item:", item_name)
+    print("Price: $", price)
+    print("-------------------")
+
 def addingItems():
     while Checkout:
         for item in foodList:
             print(item)
-        choice = input("Enter the number of the item you would like : ")
+        choice = int(input("Enter the number of the item you would like: "))
+
         if choice == 1:
+            item_name = "Meatpie"
+            price = 5
             print("You chose the Meatpie")
+            receipt("Meatpie", 5)
+            break
         elif choice == 2:
+            item_name = "Scroll"
+            price = 10
             print("You chose the scroll")
+            receipt("Scroll", 10)
+            break
         elif choice == 3:
+            item_name = "Juice"
+            price = 3
             print("You chose the juice")
-        elif choice == 4:
-            print("Would you like to go to the checkout?")
-#            checkoutChoice = input("Would you like to go to the Checkout? 1 for yes, 2 for no")
-#            if checkoutChoice ==  1:
-#                break   
-      
+            receipt("Juice", 3)
+            break
+        else:
+            print("Invalid choice")
+            return
 
 addingItems()
